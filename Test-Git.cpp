6593ee5da4cfadec117967d43ba2386cb9c965bc
Codex-0x1 <codex0x09@gmail.com>
2023-02-28 17:46:25 +0200

    add if section to the main file (Test-Git.cpp)

---
+++ Test-Git.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+	/*
+	 *my first Cpp program to test my new work flow Git & gitHub & IDE
+	 */
+
+
+int main()
+{
+	int var1 = 0, var2 = 0, var3 = 0;
+	cout << var3 << endl
+	     << var2 << endl
+	     << var1 << endl ;
+	cout << "three Vars will be printed!";
+    if true 
+  {
+
+
+
+
+
+  }
+}
