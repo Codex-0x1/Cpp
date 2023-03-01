7f9dfcfcc9fc8eb8149c08f859a7a8e37d6648dc
Codex-0x1 <codex0x09@gmail.com>
2023-03-01 02:13:32 +0200

    Modified the C++ file

--- Test-Git.cpp
+++ Test-Git.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 
-	/*
-	 *my first Cpp program to test my new work flow Git & gitHub & IDE
-	 */
-
+/*
+ *my first Cpp program to test my new work flow Git & gitHub & IDE
+ */
 
 int main()
 {
-	int var1 = 0, var2 = 0, var3 = 0;
-	cout << var3 << endl
-	     << var2 << endl
-	     << var1 << endl ;
-	cout << "three Vars will be printed!";
-    if true 
+  int var1 = 0, var2 = 0, var3 = 0;
+  cout << var3 << endl
+    << var2 << endl
+    << var1 << endl ;
+  cout << "three Vars will be printed!";
+  if( var1 == var2 )
   {
-
-
-
-
-
+    cout<<"var1 is equal to var2 \n";
+  }
+  else
+  {
+    cout<<"var1 is not equal to var2 \n";
   }
 }
